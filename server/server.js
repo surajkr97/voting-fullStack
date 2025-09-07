a10b949205a819c9104485816e977943dd715e3d
Suraj Kumar <surajkumar06174@gmail.com>
2025-09-08 02:17:06 +0530

    fix: cors for env.url and *

--- server/server.js
+++ server/server.js
@@ -7,7 +7,10 @@ const bodyParser = require('body-parser');
 const db = require('./db');
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL || '*',
+  credentials: true,
+}));
 app.use(bodyParser.json());
 
 // Import routers
